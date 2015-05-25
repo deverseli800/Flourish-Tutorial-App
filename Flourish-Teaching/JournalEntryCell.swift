f8ef02fd06f4d44e30f5b3fd925d1e7db268ae75
deverseli800 <cesar.devers@gmail.com>
2015-05-25 14:50:17 -0400

    bottom border table cell

--- Flourish-Teaching/JournalEntryCell.swift
+++ Flourish-Teaching/JournalEntryCell.swift
@@ -35,4 +35,35 @@ class JournalEntryCell: UITableViewCell
         }
     }
     
+    //For views that contain custom content using UIKit or Core Graphics, the system calls the view’s drawRect: method. 
+    override func drawRect(rect: CGRect)
+    {
+        if let mood = entry?.mood
+        {
+            // Draw bottom border
+            let bottomPath = UIBezierPath()
+            //The moveToPoint: method sets the starting point of the shape you want to create.
+            bottomPath.moveToPoint(CGPoint(x: 0, y: contentView.bounds.height))
+            //you create the lines of the shape using the addLineToPoint: method
+            bottomPath.addLineToPoint(CGPoint(x: contentView.bounds.width, y: contentView.bounds.height))
+            
+            
+            
+            let borderBottom = CAShapeLayer()
+            // The path defining the shape to be rendered. Animatable.
+            borderBottom.path = bottomPath.CGPath
+            // Specifies the line width of the shape’s path
+            borderBottom.lineWidth = 1.0
+            // The color used to stroke the shape’s path.
+            borderBottom.strokeColor = UIColor(rgba: "#dad5cd").CGColor
+            // stacking the layer in the z index, from CA layer
+            borderBottom.zPosition = 2
+            // add layer
+            contentView.layer.addSublayer(borderBottom)
+            
+           
+        }
+    }
+
+    
     }
